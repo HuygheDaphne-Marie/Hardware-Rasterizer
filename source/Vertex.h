edd375d7767f8cf0403bb9949d0409168a268cdd
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2022-01-03 16:58:34 +0100

    Mesh loaded and displaying

--- source/Vertex.h
+++ source/Vertex.h
@@ -6,7 +6,7 @@ struct IVertex
 {
 	Elite::FPoint3 position;
 	Elite::FPoint2 uv;
-	Elite::RGBColor color;
+	Elite::RGBColor color{1.0f, 1.0f, 1.0f};
 };
 
 //struct OVertex
--- source/main.cpp
+++ source/main.cpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include "SceneManager.h"
 #include "Texture.h"
+#include "EOBJParser.h"
 
 using namespace Elite;
 
@@ -49,18 +50,26 @@ int main(int argc, char* args[])
 	SceneGraph& activeScene = sceneManager.GetActiveScene();
 	activeScene.SetCamera(new Camera(width, height));
 	{
-		const std::vector<IVertex> vertices // quad
-		{
-			{FPoint3{-3.f, 3.f, 2.f}, FPoint2{0,0}, RGBColor{1.f, 1.f, 1.f}},
-			{FPoint3{3.f, 3.f, 2.f}, FPoint2{1,0}, RGBColor{1.f, 1.f, 1.f}},
-			{FPoint3{-3.f, -3.f, 2.f}, FPoint2{0,1}, RGBColor{1.f, 1.f, 1.f}},
-			{FPoint3{3.f, -3.f, 2.f}, FPoint2{1,1}, RGBColor{1.f, 1.f, 1.f}}
-		};
-		const std::vector<uint32_t> indices{ 0, 1, 2, 2, 1, 3 };
+		//const std::vector<IVertex> vertices // quad
+		//{
+		//	{FPoint3{-3.f, 3.f, 2.f}, FPoint2{0,0}, RGBColor{1.f, 1.f, 1.f}},
+		//	{FPoint3{3.f, 3.f, 2.f}, FPoint2{1,0}, RGBColor{1.f, 1.f, 1.f}},
+		//	{FPoint3{-3.f, -3.f, 2.f}, FPoint2{0,1}, RGBColor{1.f, 1.f, 1.f}},
+		//	{FPoint3{3.f, -3.f, 2.f}, FPoint2{1,1}, RGBColor{1.f, 1.f, 1.f}}
+		//};
+		//const std::vector<uint32_t> indices{ 0, 1, 2, 2, 1, 3 };
+		//
+		//Mesh* pQuad = new Mesh(pRenderer->GetDevice(), vertices, indices, L"Resources/PosCol3D.fx");
+		//pQuad->SetDiffuseTexture(new Texture("Resources/uv_grid_2.png", pRenderer->GetDevice()));
+		//activeScene.AddGeometryToScene(pQuad);
 
-		Mesh* pQuad = new Mesh(pRenderer->GetDevice(), vertices, indices, L"Resources/PosCol3D.fx");
-		pQuad->SetDiffuseTexture(new Texture("Resources/uv_grid_2.png", pRenderer->GetDevice()));
-		activeScene.AddGeometryToScene(pQuad);
+		std::vector<IVertex> verticesFromFile{};
+		std::vector<uint32_t> indices{};
+		ParseOBJ("Resources/vehicle.obj", verticesFromFile, indices);
+
+		Mesh* pMesh = new Mesh(pRenderer->GetDevice(), verticesFromFile, indices, L"Resources/PosCol3D.fx");
+		pMesh->SetDiffuseTexture(new Texture("Resources/vehicle_diffuse.png", pRenderer->GetDevice()));
+		activeScene.AddGeometryToScene(pMesh);
 	}
 	
 	//Start loop
